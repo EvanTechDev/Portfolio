852593c86ee99f875db0bb4c566ea9185cabbde6
Evan Huang <evan.huang000@proton.me>
2025-04-04 18:14:46 +0800

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -78,7 +78,7 @@ export const DATA = {
     { href: "/", icon: HomeIcon, label: "Home" },
     { href: "/blog", icon: NotebookIcon, label: "Blog" },
     { href: "/projects", icon: FolderIcon, label: "Projects" },
-    { href: "/gadgets", icon: Icons.shop, label: "Gadgets" },
+    { href: "/gadgets", icon: Icons.shop, label: "Cart" },
   ],
   contact: {
     email: "evan.huang000@proton.me",
