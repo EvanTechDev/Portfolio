5d35608c9320e5c527fa023d2de4751162252167
Evan Huang <evan.huang000@proton.me>
2025-04-09 22:16:55 +0800

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh] space-y-4">
-      <h2 className="text-2xl font-bold">Not Found</h2>
-      <p className="text-muted-foreground">Could not find the requested resource</p>
+      <h2 className="text-2xl font-bold">Page Not Found</h2>
+      <p className="text-muted-foreground">It's not our fault, it's your fault</p>
       <Link 
         href="/"
         className="underline underline-offset-4 hover:text-primary"
