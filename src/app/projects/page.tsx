3f4218af4d4b7148e680ab1232bcff87efacf2f1
Evan Huang <evan.huang000@proton.me>
2025-04-13 11:43:47 +0800

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -18,6 +18,13 @@ const ProjectCard = dynamic(() => import("@/components/project-card").then(mod =
 export default function ProjectsPage() {
   return (
     <section>
+      <div 
+      className="relative min-h-screen bg-background"
+      style={{
+        backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23595959' fill-opacity='0.05'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+        backgroundSize: '30px 30px',
+      }}
+    >
       <BlurFade delay={BLUR_FADE_DELAY}>
         <h1 className="font-medium text-2xl mb-8 tracking-tighter">projects</h1>
       </BlurFade>
@@ -35,6 +42,7 @@ export default function ProjectsPage() {
             </div>
           </BlurFade>
         ))}
+      </div>
       </div>
     </section>
   );
