7b112fc47cb1da5b8734bccd333e41a9e242311b
Evan Huang <evan.huang000@proton.me>
2025-07-17 19:08:32 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -265,13 +265,13 @@ export default function Page() {
               <div className="flex flex-col items-center justify-center space-y-4 text-center">
                 <div className="space-y-2">
                   <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                    Milestone
+                    Hackathons
                   </div>
                   <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
                     I like coding.
                   </h2>
                   <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                    This is my personal milestone.
+                    Hackathons I’ve participated in
                   </p>
                 </div>
               </div>
