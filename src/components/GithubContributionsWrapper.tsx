971eb168003dfd78b980f59dd001b8df32677d4e
Evan Huang <evan.huang000@proton.me>
2025-12-07 08:37:28 +0800

    Create GithubContributionsWrapper.tsx

---
+++ src/components/GithubContributionsWrapper.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import GithubSkeleton from "@/components/ui/skeleton";
+
+const GithubContributions = dynamic(
+  async () => {
+    const mod = await import("@/components/github-calendar");
+    return mod.GithubContributions;
+  },
+  {
+    ssr: false,
+    loading: () => <GithubSkeleton />,
+  }
+);
+
+export default function GithubContributionsWrapper() {
+  return <GithubContributions />;
+}
