3bd5c8f6674ddb7cabed9e9b09dda2c2d84606ff
Evan Huang <evan.huang000@proton.me>
2025-12-07 08:58:26 +0800

    Update layout.tsx

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -1,24 +1,14 @@
-import { ReactNode } from "react";
 import { Metadata } from "next";
 import { DATA } from "@/data/resume";
 
 export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
   const canonicalUrl = `${DATA.url}/blog/${params.slug}`;
-
   return {
     metadataBase: new URL(DATA.url),
-    alternates: {
-      canonical: canonicalUrl,
-    },
+    alternates: { canonical: canonicalUrl },
   };
 }
 
-export default function BlogLayout({
-  children,
-  params,
-}: {
-  children: ReactNode;
-  params: { slug: string };
-}) {
-  return <>{children}</>;
+export default function BlogLayout(props: { children: React.ReactNode; params: { slug: string } }) {
+  return <>{props.children}</>;
 }
