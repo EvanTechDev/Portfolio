ed3b527e15e4c6df2b2eac1138ea317cd108d392
Evan Huang <evan.huang000@proton.me>
2025-12-13 15:13:21 +0800

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -1,7 +1,7 @@
 import { Icons } from "@/components/icons";
 import { Description } from "@radix-ui/react-dialog";
 import { url } from "inspector";
-import { HomeIcon, NotebookIcon, ThumbsDown, VideoIcon, FolderIcon, Medal } from "lucide-react";
+import { HomeIcon, NotebookIcon, ThumbsDown, VideoIcon, FolderIcon, Medal, Terminal } from "lucide-react";
 import { title } from "process";
 
 export const DATA = {
@@ -67,7 +67,8 @@ export const DATA = {
     { href: "/", icon: HomeIcon, label: "Home" },
     { href: "/blog", icon: NotebookIcon, label: "Blog" },
     { href: "/projects", icon: FolderIcon, label: "Projects" },
-    { href: "/certificates", icon: Medal, label: "Certficates" }
+    { href: "/certificates", icon: Medal, label: "Certficates" },
+    { href: "/cli", icon: Terminal, label: "CLI" }
   ],
   contact: {
     email: "evan.huang000@proton.me",
