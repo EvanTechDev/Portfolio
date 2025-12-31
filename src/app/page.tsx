55664cbe2eef9d7b463926471106928eb9f34899
Evan Huang <evan.huang000@proton.me>
2025-12-31 21:57:34 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -161,12 +161,13 @@ export default function Page() {
 
         <section id="skills">
           <div className="flex min-h-0 flex-col gap-y-3">
-            <BlurFade delay={BLUR_FADE_DELAY * 9}>
               <h2 className="text-xl font-bold">Skills</h2>
             </BlurFade>
             <div className="flex flex-wrap gap-1">
-              <img src="https://skills-icons.vercel.app/api/icons?perline=5&i=nextjs,react,typescript,javascript,tailwindcss,nodejs,postgresql,supabase,git,cloudflare" alt="Skills" />
-              {DATA.skills.map((skill, id) => (
+              <BlurFade delay={BLUR_FADE_DELAY * 10}>
+                <img src="https://skills-icons.vercel.app/api/icons?perline=5&i=nextjs,react,typescript,javascript,tailwindcss,nodejs,postgresql,supabase,git,cloudflare" alt="Skills" />
+              </BlurFade>
+                {DATA.skills.map((skill, id) => (
                 <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
                   <Badge key={skill}>{skill}</Badge>
                 </BlurFade>
