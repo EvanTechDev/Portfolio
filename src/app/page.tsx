905928241cab25409f67aad43268bbc4f677a076
Evan Huang <evan.huang000@proton.me>
2025-12-31 22:02:14 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -161,6 +161,7 @@ export default function Page() {
 
         <section id="skills">
           <div className="flex min-h-0 flex-col gap-y-3">
+            <BlurFade delay={BLUR_FADE_DELAY * 9}>
               <h2 className="text-xl font-bold">Skills</h2>
             </BlurFade>
             <div className="flex flex-wrap gap-1">
